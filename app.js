072fe3f7c52a8f706287dd255362966c66d9ef57
Daniil Ryabenkov <danilryaba0@gmail.com>
2026-03-03 20:45:44 +0500

    add enemy damage logic(hearts)

--- app.js
+++ app.js
@@ -36,10 +36,14 @@ let hit = false;
 let jump = false;
 let fall = false;
 
+let maxLives = 6;
+let lives = 6; // текущая жизнь
+
 let heroX = Math.floor((parseInt(imgBlock.style.left) + 32, 10) / 32);
 let heroY = Math.floor(parseInt(imgBlock.style.bottom, 10) / 32);
 
 let tileArray = [];
+let heartsArray = [];
 
 jumpBlock.onclick = () => (jump = true);
 hitBlock.onclick = () => (hit = true);
@@ -49,6 +53,7 @@ const updateHeroXY = () => {
   heroY = Math.ceil(parseInt(imgBlock.style.bottom) / 32);
   //console.log(heroX, heroY); // почему-то дважды heroX делается!
 };
+
 // НОГИ НЕМНОГО УШЛИ ПОД ТЕКСТУРУ С КАЖДЫМ ПРЫЖКОМ ВСЕ БОЛЬШЕ!
 const checkFalling = (x, y) => {
   updateHeroXY();
@@ -212,21 +217,48 @@ window.onmouseup = onTouchEnd;
 window.ontouchend = onTouchEnd;
 
 class Enemy {
+  ATTACK = "attack";
+  DEATH = "death";
+  HURT = "hurt";
+  IDLE = "idle";
+  WALK = "walk";
+
+  state; // для состояния врага
+  animateWasChanged;
+
   posX;
   posY;
-  img;
-  block;
   blockSize;
   spritePos; // позиция спрайта на данный момент
   spriteMaxPos;
+
+  startX;
+  dir;
+
+  stop;
+
+  img;
+  block;
   timer;
+
+  soucePath;
+
   constructor(x, y) {
     this.posX = x;
+    this.startX = this.posX;
+    this.dir = 1;
+    this.stop = false;
     this.posY = y;
     this.blockSize = 96;
     this.spritePos = 0;
     this.spriteMaxPos = 3;
+    this.soucePath = "assets/Enemy/1/";
+
+    this.state = this.IDLE;
+    this.animateWasChanged = false;
+
     this.#createImg();
+    this.changeAnimate(this.WALK);
     this.#lifeCycle();
   }
   #createImg() {
@@ -239,24 +271,142 @@ class Enemy {
     this.block.style.overflow = "hidden";
 
     this.img = document.createElement("img");
-    this.img.src = "assets/Enemy/1/Idle.png";
+    this.img.src = this.soucePath + "Idle.png";
     this.img.style.position = "absolute";
     this.img.style.left = 0;
     this.img.style.bottom = 0;
-    this.img.style.width = `${this.blockSize * 4}px`;
+    this.img.style.width = `${this.blockSize * this.state.spriteMaxPos}px`;
     this.img.style.height = `${this.blockSize}px`;
     this.block.appendChild(this.img);
     canvas.appendChild(this.block);
   }
   #lifeCycle() {
     this.timer = setInterval(() => {
+      if (this.animateWasChanged) {
+        this.animateWasChanged = false;
+        switch (this.state) {
+          case this.ATTACK: {
+            this.setAttack();
+            break;
+          }
+          case this.HURT: {
+            this.setHurt();
+            break;
+          }
+          case this.DEATH: {
+            this.setDeath();
+            break;
+          }
+          case this.WALK: {
+            this.setWalk();
+            break;
+          }
+          case this.IDLE: {
+            this.setIdle();
+            break;
+          }
+          default:
+            break;
+        }
+      }
       this.spritePos++;
+      this.checkCollide();
+      if (!this.stop) {
+        this.move();
+      } else {
+        this.changeAnimate(this.ATTACK);
+      }
       this.#animate();
     }, 140);
   }
   #animate() {
-    if (this.spritePos > this.spriteMaxPos) this.spritePos = 0;
+    if (this.spritePos > this.spriteMaxPos) {
+      this.spritePos = 0;
+      if (this.state === this.ATTACK) {
+        lives--;
+        updateHearts();
+      }
+    }
     this.img.style.left = `-${this.spritePos * this.blockSize}px`;
+  }
+  setAttack() {
+    this.img.src = this.soucePath + "Attack.png";
+    this.spriteMaxPos = 5;
+  }
+  setDeath() {
+    this.img.src = this.soucePath + "Death.png";
+    this.spriteMaxPos = 5;
+  }
+  setHurt() {
+    this.img.src = this.soucePath + "Hurt.png";
+    this.spriteMaxPos = 1;
+  }
+  setIdle() {
+    this.img.src = this.soucePath + "Idle.png";
+    this.spriteMaxPos = 3;
+  }
+  setWalk() {
+    this.img.src = this.soucePath + "Walk.png";
+    this.spriteMaxPos = 5;
+  }
+  changeAnimate(localState) {
+    this.state = localState;
+    this.animateWasChanged = true;
+  }
+  move() {
+    if (this.posX > this.startX + 10) {
+      this.dir *= -1;
+      this.img.style.transform = "scale(-1,1)";
+    } else if (this.posX <= this.startX) {
+      this.dir = Math.abs(this.dir);
+      this.img.style.transform = "scale(1,1)";
+    }
+    this.posX += this.dir;
+    this.block.style.left = `${this.posX * 32}px`;
+  }
+  checkCollide() {
+    if (heroY === this.posY) {
+      if (heroX === this.posX) {
+        this.stop = true;
+      } else if (heroX === this.posX + 2) {
+        this.stop = true;
+        //
+      } else {
+        this.stop = false;
+        this.changeAnimate(this.WALK);
+      }
+    } else {
+      this.stop = false;
+      this.changeAnimate(this.WALK);
+    }
+  }
+}
+
+class Heart {
+  img;
+  x;
+  constructor(x, src) {
+    this.x = x + 1;
+    this.img = document.createElement("img");
+    this.img.src = src;
+    this.img.style.position = "absolute";
+    this.img.style.left = `${this.x * 32}px`;
+    this.img.style.bottom = (window.screen.height / 32 - 2) * 32;
+    this.img.style.width = 32;
+    this.img.style.height = 32;
+    canvas.appendChild(this.img);
+  }
+}
+
+class HeartEmpty extends Heart {
+  constructor(x) {
+    super(x, "assets/Hearts/heart_empty.png");
+  }
+}
+
+class HeartRed extends Heart {
+  constructor(x) {
+    super(x, "assets/Hearts/heart_red.png");
   }
 }
 
@@ -286,15 +436,35 @@ const addTiles = (i) => {
   canvas.appendChild(tileBlack);
 };
 
+const addHearts = () => {
+  for (let i = 0; i < maxLives; i++) {
+    let heartRed = new HeartRed(i);
+    let heartEmpty = new HeartEmpty(i);
+    heartsArray.push(heartRed);
+  }
+};
+
+const updateHearts = () => {
+  if (lives < 1) lives = 1;
+  for (let i = 0; i < lives; i++) {
+    heartsArray[i].img.style.display = "block";
+  }
+  for (let i = lives; i < maxLives; i++) {
+    heartsArray[i].img.style.display = "none";
+  }
+};
+
 const start = () => {
   lifeCycle();
   for (let i = 0; i < window.screen.width / 32; i++) {
-    if (i > 10 && i < 15) continue;
-    else addTiles(i);
+    addTiles(i);
   }
   createTilePlatform(10, 10, 10);
   createTilePlatform(5, 5, 10);
   let enemy = new Enemy(10, 2);
+
+  addHearts();
+  updateHearts();
 };
 
 start();
